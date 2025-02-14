96b4670cbbd303b36c51d17adf42ed4b958a88e9
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-14 12:16:28 -0800

    Defining eks module

---
+++ modules/eks/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the EKS cluster will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs where the EKS nodes will be deployed"
+  type        = list(string)
+}
+
+variable "eks_role_arn" {
+  description = "IAM Role ARN for the EKS cluster"
+  type        = string
+}
