837cff2560ce5499f5c6193cc5a93fac2b36c6bf
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-14 10:05:52 -0800

    Adding 'cluster_name' Variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "The AWS region where resources will be deployed"
   type        = string
 }
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
