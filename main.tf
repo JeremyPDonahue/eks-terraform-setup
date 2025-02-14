c35e6338a72bcb15b3161d4e3c2bfaadcd8f81ec
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-14 14:52:01 -0800

    Defined Main Module

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+module "vpc" {
+  source   = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
+}
+
+module "iam" {
+  source       = "./modules/iam"
+  cluster_name = var.cluster_name
+}
+
+module "eks" {
+  source          = "./modules/eks"
+  cluster_name    = var.cluster_name
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.public_subnet_ids
+  eks_role_arn    = module.iam.eks_role_arn
+}
+
+module "worker_nodes" {
+  source       = "./modules/worker_nodes"
+  cluster_name = var.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.public_subnet_ids
+  node_role_arn = module.iam.node_role_arn
+}
