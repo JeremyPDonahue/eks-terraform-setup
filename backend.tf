a71668212d4934e02ca5608869bc500fe9b3f80a
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-14 18:25:07 -0800

    Configure S3 Bucket

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "js-test-cluster-state-bucket"
+    key            = "eks-cluster/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "js-lock-table"
+  }
+}
