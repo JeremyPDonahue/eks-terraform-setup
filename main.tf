4422b2632536e825692b2bbd14a727c4c6efc528
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-23 11:05:10 -0800

    Terraform fmt

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ module "iam" {
 }
 
 module "eks" {
-  source          = "./modules/eks"
-  cluster_name    = var.cluster_name
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.public_subnet_ids
-  eks_role_arn    = module.iam.eks_role_arn
-}
-
-module "worker_nodes" {
-  source       = "./modules/worker_nodes"
+  source       = "./modules/eks"
   cluster_name = var.cluster_name
   vpc_id       = module.vpc.vpc_id
   subnet_ids   = module.vpc.public_subnet_ids
+  eks_role_arn = module.iam.eks_role_arn
+}
+
+module "worker_nodes" {
+  source                = "./modules/worker_nodes"
+  cluster_name          = var.cluster_name
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.public_subnet_ids
   worker_nodes_role_arn = module.iam.worker_nodes_role_arn
 }
 
