a26a1026448ace2d4be241cf8e04f2bdb69ed45e
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-23 15:50:22 -0800

    Ref subnets in eks

--- main.tf
+++ main.tf
@@ -13,7 +13,10 @@ module "eks" {
   source       = "./modules/eks"
   cluster_name = var.cluster_name
   vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.public_subnet_ids
+  subnet_ids   = [
+    "subnet-05a2a065ed0df87ad", # us-west-2d
+    "subnet-0cfd1c548c66d072e"  # us-west-2b
+  ]
   eks_role_arn = module.iam.eks_role_arn
 }
 
