a1fe4bbe474fa71e9c7f7e78f8194f0bf379528c
jeremy.donahue <jeremydonahue@protonmail.com>
2025-02-26 08:01:11 -0800

    Switch back to pull_request_target

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,11 +3,6 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "aws_region" {
-  description = "The AWS region where resources will be deployed"
-  type        = string
-}
-
 variable "vpc_id" {
   description = "The VPC ID where the EKS cluster will be deployed"
   type        = string
@@ -17,7 +12,6 @@ variable "subnet_ids" {
   type    = list(string)
   default = ["subnet-018a047e6e74acc93", "subnet-0cf8dd5bd2868fff8"]
 }
-
 
 variable "eks_role_arn" {
   description = "IAM Role ARN for the EKS cluster"
