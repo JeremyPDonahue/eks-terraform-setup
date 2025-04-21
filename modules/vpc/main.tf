1cddc0fa83f3e210260a43d95cf87aeca9a175fb
jeremy.donahue <jeremydonahue@protonmail.com>
2025-04-21 15:13:38 -0700

    Formatted Files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "eks_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -9,9 +9,9 @@ resource "aws_vpc" "eks_vpc" {
 }
 
 resource "aws_subnet" "public" {
-  count = 3
-  vpc_id = aws_vpc.eks_vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 4, count.index + 2)
+  count                   = 3
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index + 2)
   map_public_ip_on_launch = true
 
   tags = {
